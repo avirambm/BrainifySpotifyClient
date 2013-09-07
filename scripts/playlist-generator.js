83beb160a3a9170f2cf41dd015f57e9637c2ed5b
Aviram Ben Moshe <abmstudios@gmail.com>
2013-09-06 22:11:32 -0400

    1. Bug fix: now generating random playlist name (a static name didn't work well with multiple playlists). 2. Prepared the cover drawing mechanism.

--- scripts/playlist-generator.js
+++ scripts/playlist-generator.js
@@ -1,3 +1,13 @@
+function generatePlaylistTempId() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 32; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 require([
     '$api/models',
     '$views/list#List',
@@ -8,7 +18,7 @@ require([
     var generatePlaylistFromTracks = function(playlistContainer, coverContainer, playlistTracks) {
 
         var playlist_name = generatePlaylistTempId();
-        models.Playlist.createTemporary('tempPlaylist').done(function(playlist) {
+        models.Playlist.createTemporary(playlist_name).done(function(playlist) {
             playlist.load('tracks').done(function(loadedPlaylist) {
                 var length = playlistTracks.length;
                 for (var i = 0; i < length; i++) {
@@ -19,6 +29,12 @@ require([
             var list = List.forPlaylist(playlist, { style: 'rounded' });
             document.getElementById(playlistContainer).appendChild(list.node);
             list.init();
+
+            for (var i = 0; i < 4; i++) {
+                var track = models.Track.fromURI(playlistTracks[i]);
+                var image = Image.forTrack(track, { width:75, height:75 });
+                document.getElementById(coverContainer + (i+1).toString()).appendChild(image.node);
+            }
         });
         models.Playlist.removeTemporary(playlist_name);
     };
