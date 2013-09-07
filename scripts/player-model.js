2a5f704e176df3d8c8a771a24a23c2182e20356f
benzvi <benzvi.origit@gmail.com>
2013-09-07 09:45:30 +0300

    Added functionality to player-model.js. it now sends to the server whenever there is a change in the player (for example, when a song is paused, skipped etc.)

--- scripts/player-model.js
+++ scripts/player-model.js
@@ -3,25 +3,84 @@ require([
 ], function(models) {
     'use strict';
 
+    var currTrack = '';
+    var tempTrack = '';
+
     var doGetCurrentTrackOnChangeEvent = function () {
-        models.player.addEventListener('change', function() {
-            getCurrentSong();
+        models.player.addEventListener('change:track', function() {
+            getCurrentSong(0);
+        });
+
+        models.player.addEventListener('change:playing', function() {
+            getCurrentSong(1);
         });
     }
 
     var doGetCurrentTrackOnLoad = function () {
-        getCurrentSong();
+        getCurrentSong(0);
     }
 
-    function getCurrentSong() {
+    function getCurrentSong(type) {
         models.player.load('track').done(function(prop) {
+
+            tempTrack = currTrack;
+            currTrack = models.player.track;
+            var isPlaying = models.player.playing;
 
             // put track on the document only if it is not null
             if(prop.track !=null) {
                 jQuery('#nowplaying_title').html(prop.track.name);
                 jQuery('#nowplaying_artist').html(prop.track.artists[0].name);
+
+                // Notify server on song changed
+                switch(type) {
+
+                    // change track
+                    case 0:
+                        notifyTrackChange(tempTrack, false, 0);
+                        notifyTrackChange(currTrack, isPlaying, 0);
+                        break;
+
+                    // change playing
+                    case 1:
+                        notifyTrackChange(currTrack, isPlaying, 0);
+                        break;
+                }
             }
         } );
+
+
+    }
+
+    function notifyTrackChange(currTrackID, isPlaying, user_id) {
+        $.post(apiBaseUrl + "spotify/" + user_id, { "current_track_id": currTrackID.toString(), "is_playing": isPlaying }, function(data) {
+            var globalMeditation = data.global.meditation;
+            var globalEngagement = data.global.engagement;
+            var globalFrustration = data.global.happiness;
+            var globalExcitement = data.global.excitement;
+
+            var userMeditation = data.user.meditation;
+            var userEngagement = data.user.engagement;
+            var userFrustration = data.user.happiness;
+            var userExcitement = data.user.excitement;
+
+
+            // Update interface with given data about current song
+            $('#userEnergy').html(userExcitement.toFixed(1).toString());
+            $('#worldEnergy').html(globalExcitement.toFixed(1).toString());
+
+            $('#userHappiness').html(userExcitement.toFixed(1).toString());
+            $('#worldHappiness').html(globalExcitement.toFixed(1).toString());
+
+            $('#userFocus').html(userExcitement.toFixed(1).toString());
+            $('#worldFocus').html(globalExcitement.toFixed(1).toString());
+
+            $('#userCalmness').html(userExcitement.toFixed(1).toString());
+            $('#worldCalmness').html(globalExcitement.toFixed(1).toString());
+
+
+
+        }, 'json');
     }
 
     exports.doGetCurrentTrackOnChangeEvent = doGetCurrentTrackOnChangeEvent;
