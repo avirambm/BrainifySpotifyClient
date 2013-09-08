706608e935dff823c267eef454f9156eb2bc762c
benzvi <benzvi.origit@gmail.com>
2013-09-08 09:30:04 +0300

    Brainify is now integrated with Shop your way!

--- scripts/server.js
+++ scripts/server.js
@@ -160,6 +160,47 @@ require([
                     var rank = happiness[i].rank;
                     var score = happiness[i].score;
                     happinessIds.push(track_id);
+
+
+                    // For Shop Your Way
+                    var currTrack;
+                    var currAlbum;
+                    var currArtist;
+
+
+                    if(i<3) {
+                        currTrack = models.Track.fromURI(track_id);
+                        currAlbum = currTrack.album.name;
+                        currArtist = currTrack.album.artists[0].name;
+
+                        currAlbum = currAlbum.split(' ').join('%20');
+                        currArtist = currArtist.split(' ').join('%20');
+
+
+                        var happy_category = 5565530;
+
+                        var search_product_http_request = "http://sandboxplatform.shopyourway.com/search/products?q=" + currArtist + "%20" + currAlbum +
+                            "&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9";
+
+                        // execute get request and put result in product_json_result
+                        $.getJSON(search_product_http_request , function(product_json_result) {
+
+                            // No results.
+                            if(product_json_result.length == 0) {
+                                console.log("Info: Didn't find product id for " + currArtist + "%20" + currAlbum);
+                                return;
+                            }
+                            var product_id = product_json_result[0].id;
+
+                            var tag_product_http_request =   "http://sandboxplatform.shopyourway.com/tags/tag?type=product&targetid=" + product_id + "&tagids=" + happy_category + "&by=app&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9"
+
+                            $.getJSON(tag_product_http_request , function(product_json_result) {
+                                           console.log(product_json_result + " " + product_id);
+
+                            });
+
+                        });
+                    }
                 }
                 playlistGenerator.generatePlaylistFromTracks('firstPlaylistContainer', 'firstAlbumCoverContainer', happinessIds);
 
@@ -171,6 +212,43 @@ require([
                     var rank = engagement[i].rank;
                     var score = engagement[i].score;
                     engagementIds.push(track_id);
+
+
+                    // For Shop Your Way
+
+                    if(i<3) {
+                        currTrack = models.Track.fromURI(track_id);
+                        currAlbum = currTrack.album.name;
+                        currArtist = currTrack.album.artists[0].name;
+
+                        currAlbum = currAlbum.split(' ').join('%20');
+                        currArtist = currArtist.split(' ').join('%20');
+
+                        var focus_category = 5565529;
+
+                        var search_product_http_request = "http://sandboxplatform.shopyourway.com/search/products?q=" + currArtist + "%20" + currAlbum +
+                            "&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9";
+
+                        // execute get request and put result in product_json_result
+                        $.getJSON(search_product_http_request , function(product_json_result) {
+
+                            // No results.
+                            if(product_json_result.length == 0) {
+                                console.log("Info: Didn't find product id for " + currArtist + "%20" + currAlbum);
+                                return;
+                            }
+                            var product_id = product_json_result[0].id;
+
+                            var tag_product_http_request =   "http://sandboxplatform.shopyourway.com/tags/tag?type=product&targetid=" + product_id + "&tagids=" + focus_category + "&by=app&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9"
+
+                            $.getJSON(tag_product_http_request , function(product_json_result) {
+                                console.log(product_json_result + " " + "http://sandbox.shopyourway.com/Brainify/" + product_id);
+
+                            });
+
+                        });
+                    }
+
                 }
                 playlistGenerator.generatePlaylistFromTracks('secondPlaylistContainer', 'secondAlbumCoverContainer', engagementIds);
 
@@ -182,6 +260,43 @@ require([
                     var rank = meditation[i].rank;
                     var score = meditation[i].score;
                     meditationIds.push(track_id);
+
+                    // For Shop Your Way
+
+                    if(i<3) {
+                        currTrack = models.Track.fromURI(track_id);
+                        currAlbum = currTrack.album.name;
+                        currArtist = currTrack.album.artists[0].name;
+
+                        currAlbum = currAlbum.split(' ').join('%20');
+                        currArtist = currArtist.split(' ').join('%20');
+
+                        var relaxing_category = 5565559;
+
+                        var search_product_http_request = "http://sandboxplatform.shopyourway.com/search/products?q=" + currArtist + "%20" + currAlbum +
+                            "&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9";
+
+                        // execute get request and put result in product_json_result
+                        $.getJSON(search_product_http_request , function(product_json_result) {
+
+                            // No results.
+                            if(product_json_result.length == 0) {
+                                console.log("Info: Didn't find product id for " + currArtist + "%20" + currAlbum);
+                                return;
+                            }
+                            var product_id = product_json_result[0].id;
+
+                            tag_product_http_request =   "http://sandboxplatform.shopyourway.com/tags/tag?type=product&targetid=" + product_id + "&tagids=" + relaxing_category + "&by=app&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9";
+
+                            $.getJSON(tag_product_http_request , function(product_json_result) {
+                                console.log(product_json_result + " " + "http://sandbox.shopyourway.com/Brainify/" + product_id);
+
+                            });
+
+                        });
+                    }
+
+
                 }
                 playlistGenerator.generatePlaylistFromTracks('thirdPlaylistContainer', 'thirdAlbumCoverContainer', meditationIds);
 
@@ -193,6 +308,41 @@ require([
                     var rank = excitement[i].rank;
                     var score = excitement[i].score;
                     wakeIds.push(track_id);
+
+                    // For Shop Your Way
+
+                    if(i<3) {
+                        currTrack = models.Track.fromURI(track_id);
+                        currAlbum = currTrack.album.name;
+                        currArtist = currTrack.album.artists[0].name;
+
+                        currAlbum = currAlbum.split(' ').join('%20');
+                        currArtist = currArtist.split(' ').join('%20');
+
+                        var wake_category = 5565558;
+
+                        var search_product_http_request = "http://sandboxplatform.shopyourway.com/search/products?q=" + currArtist + "%20" + currAlbum +
+                            "&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9";
+
+                        // execute get request and put result in product_json_result
+                        $.getJSON(search_product_http_request , function(product_json_result) {
+
+                            // No results.
+                            if(product_json_result.length == 0) {
+                                console.log("Info: Didn't find product id for " + currArtist + "%20" + currAlbum);
+                                return;
+                            }
+                            var product_id = product_json_result[0].id;
+
+                            var tag_product_http_request =   "http://sandboxplatform.shopyourway.com/tags/tag?type=product&targetid=" + product_id + "&tagids=" + wake_category + "&by=app&token=0_13612_1378958228_2_3ac097e7d91a7c8a96f56c433feb5bf1940263e5cb6822fff15f773f8341eb50&hash=bf7c497bc62bd5d0ce0f49517261d3088e1c7d37e363280368657b1c72a48fe9"
+
+                            $.getJSON(tag_product_http_request , function(product_json_result) {
+                                console.log(product_json_result + " " + "http://sandbox.shopyourway.com/Brainify/" + product_id);
+
+                            });
+
+                        });
+                    }
                 }
                 playlistGenerator.generatePlaylistFromTracks('fourthPlaylistContainer', 'fourthAlbumCoverContainer', wakeIds);
             });
